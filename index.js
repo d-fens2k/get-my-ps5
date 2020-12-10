0ff7c82420c6a1d8cf8f90a7b05b092aca05f4ec
Tobias Bischof <tobi@token.net>
2020-12-09 18:06:30 -0800

    added twilio

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const open = require('open');
 const promptly = require('promptly');
+const twilio = require('twilio');
+
 
 const {checkForPlaystationDirectRedirect} = require("./utils");
 
@@ -20,9 +22,31 @@ const playstationType = {
 (async function() {
     const choice = await promptly.choose("Which version would you like? (disc or digital)", ["disc", "digital"]);
     console.log(`Searching for PlayStation 5 ${choice} edition...`);
+
+    var smsNotifications = false;
+    console.log("Twilio information required - please enter phone numbers in international format");
+    console.log("E.g. +49123456789");
+    const twilioNumber = await promptly.prompt("Twilio Phone Number: ");
+    const accountSid = await promptly.prompt("Twilio Account SID: ");
+    const authToken = await promptly.prompt("Twilio Auth Token: ");
+    const phoneNumber = await promptly.prompt("Your phone number: ");
+    var client = new twilio(accountSid, authToken);
+    
+    client.messages.create({
+        body: 'Get-My-PS5 has been configured properly and is now running',
+        to: phoneNumber,  
+        from: twilioNumber 
+    })
+    .then((message) => console.log('Twilio message ID: ', message.sid));
     
     const onSuccess = () => {
         console.log("Found it! Opening queue now...");
+        client.messages.create({
+            body: 'PS5 found!! Run to your computer to complete checkout!',
+            to: phoneNumber,  
+            from: twilioNumber 
+        })
+        .then((message) => console.log('Twilio message ID: ', message.sid));
         open(playstationType[choice].url);
     };
 
